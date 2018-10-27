1b2bb5706c9b516b29dc3b7a56f0a59e4cf53a12
BoucheDimitri <dimitri.bouche@ensae-paristech.fr>
2018-10-27 17:54:54 +0200

    Added docstrings for Kmeans

--- kmeans.py
+++ kmeans.py
@@ -5,17 +5,47 @@ import matplotlib.pyplot as plt
 
 
 def random_init(xs, k):
+    """
+    Affect the data vectors to a random cluster
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        k (int): the number of clusters
+
+    Returns:
+        np.ndarray: random vector of assignement to clusters (nsamples, )
+    """
     z = np.random.choice(np.array(range(0, k)), xs.shape[1])
     return z
 
 
 def assign_xs(xs, mus):
+    """
+    Affect the data vectors to the cluster to which centroid they are the closest
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        mus (np.ndarray): the centroids (nfeatures, nclusters)
+
+    Returns:
+        np.ndarray: vector of assignement to clusters (nsamples, )
+    """
     dists = distance.cdist(xs.T, mus.T)
     z = np.argmin(dists, axis=1)
     return z
 
 
 def update_mus(xs, z):
+    """
+    Centroids update for kmeans
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        z (np.ndarray): vector of assignement to clusters (nsamples, )
+
+    Returns:
+        np.ndarray: vector of assignement to clusters (nsamples, )
+    """
     k = np.unique(z).shape[0]
     mus = np.zeros((xs.shape[0], k))
     for i in range(0, k):
@@ -25,6 +55,17 @@ def update_mus(xs, z):
 
 
 def iterate_kmeans(xs, k, nits):
+    """
+    Iterate kmeans updates (1: centroid updates, 2: reassignement)
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        k (int): n clusters
+        nits (int): numbers of iterations to perform
+
+    Returns:
+        tuple: mus (centroid matrix), z (assignement vector)
+    """
     z = random_init(xs, k)
     for i in range(0, nits):
         mus = update_mus(xs, z)
@@ -34,12 +75,32 @@ def iterate_kmeans(xs, k, nits):
 
 
 def clustered_table(xs, z):
+    """
+    Dataset and cluster assignement in a pandas dataframe
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        z (np.ndarray): vector of assignement to clusters (nsamples, )
+
+    Returns:
+        pd.core.frame.DataFrame
+    """
     xspd = pd.DataFrame(data=xs.T, columns=["x0", "x1"])
     xspd["c"] = z
     return xspd
 
 
 def plot_clusters(xs, z):
+    """
+    Plot data in different colors according to their cluster assignement
+
+    Params:
+        xs (np.ndarray): the data matrix (nfeatures, nsamples)
+        z (np.ndarray): vector of assignement to clusters (nsamples, )
+
+    Returns:
+        nonetype: None
+    """
     xspd = clustered_table(xs, z)
     k = np.unique(z).shape[0]
     fig, ax = plt.subplots()
